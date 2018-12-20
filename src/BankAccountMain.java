57b37ddd1011143c0aef31e003dce72d6f3f5221
pwellener19 <pwellener19@HS-MATH2-29.gvsd.org>
2018-12-20 14:26:48 -0500

    Werk

--- src/BankAccountMain.java
+++ src/BankAccountMain.java
@@ -14,25 +14,62 @@ public class BankAccountMain
 		final double TRANSACTION_FEE = 1.5;
 		final double MIN_BAL = 300;
 		final double MIN_BAL_FEE = 10;
-		final double FREE_TRANSACTIONS = 10;
+		final int FREE_TRANSACTIONS = 10;
 		
 		boolean run = true;
 		while(run)
 		{
-			System.out.println("Would you like to add an account, make an account, or terminate the program? Write: 'Add', 'Make', or 'Terminate'.");
+			System.out.println("Would you like to add an account, make a transaction, or terminate the program? Write: 'Add', 'Make', or 'Terminate'.");
 			String ans = in.nextLine();
+			
+			System.out.println("What name would you like to put this account under:");
+			String name = in.next();
+			in.nextLine();
+			
 			while((!ans.equals("Add")) && (!ans.equals("Make")) && (!ans.equals("Terminate")))
 			{
 				System.out.println("Please enter a valid answer:");
 				ans = in.next();
 				in.nextLine();
 			}
+			
 			if(ans.equals("Add"))
 			{
-				System.out.println("Do you want to add a Savings or Checking Account?");
+				System.out.println("Do you want to add a Savings or Checking Account? Write: 'Savings', or 'Checking'");
+				String saveorcheck = in.next();
+				in.nextLine();
+				while((!saveorcheck.equals("Savings")) && (!saveorcheck.equals("Checking")))
+				{
+					System.out.println("Please enter a valid answer:");
+					saveorcheck = in.next();
+					in.nextLine();
+				}
 				
+				System.out.println("Would you like to make an initial deposit('Yes' or 'No'):");
+				String yorn = in.next();
+				in.nextLine();
+				while((!yorn.equals("Yes")) && (!yorn.equals("No")))
+				{
+					System.out.println("Please enter a valid answer:");
+					yorn = in.next();
+					in.nextLine();
+				}
+				if(yorn.equals("Yes"))
+				{
+					
+				}
 				
-				
+				if(yorn.equals("No"))
+				{
+					if(saveorcheck.equals("Savings"))
+					{
+						accounts.add(new SavingsAccount(name, RATE, MIN_BAL, MIN_BAL_FEE));
+					}
+					if(saveorcheck.equals("Checking"))
+					{
+						accounts.add(new CheckingAccount(name, OVER_DRAFT_FEE, TRANSACTION_FEE, FREE_TRANSACTIONS));
+					}
+				}
 				
 			}
 			
@@ -40,11 +77,9 @@ public class BankAccountMain
 			
 			if(ans.equals("Make"))
 			{
-				System.out.println("Do you want to make a Savings or Checking Account?");
-				
-				
-				
-				
+				System.out.println("What type of transaction would you like to make?: ");
+				String trans = in.next();
+				in.nextLine();
 			}
 			
 			if(ans.equals("Terminate"))
