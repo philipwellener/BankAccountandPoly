12823f92b4b85fe652c03199cbc623db4388dc29
pwellener19 <pwellener19@HS-MATH2-29.gvsd.org>
2019-01-04 14:26:23 -0500

    Error on local variable

--- src/BankAccountMain.java
+++ src/BankAccountMain.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class BankAccountMain 
 {
 	
+	//Checks whether the String is numeric
 	private static boolean isNumeric(String str)
 	{
 	try
@@ -23,6 +24,7 @@ public class BankAccountMain
 		Scanner in = new Scanner(System.in);
 		ArrayList<BankAccount> accounts = new ArrayList<BankAccount>();
 		
+		//Constants
 		final double OVER_DRAFT_FEE = 15;
 		final double RATE = .0025;
 		final double TRANSACTION_FEE = 1.5;
@@ -34,8 +36,8 @@ public class BankAccountMain
 		while(run)
 		{
 			System.out.println("Would you like to add an account, make a transaction, or terminate the program? Write: 'Add', 'Make', or 'Terminate'.");
-			String ans = in.nextLine();
-			
+			String ans = in.next();
+			in.nextLine();
 			
 			while((!ans.equals("Add")) && (!ans.equals("Make")) && (!ans.equals("Terminate")))
 			{
@@ -44,6 +46,7 @@ public class BankAccountMain
 				in.nextLine();
 			}
 			
+			//Add an Account
 			if(ans.equals("Add"))
 			{
 				System.out.println("What name would you like to put this account under:");
@@ -60,7 +63,7 @@ public class BankAccountMain
 					in.nextLine();
 				}
 				
-				
+				//With or Without Initial Deposit
 				System.out.println("Would you like to make an initial deposit('Yes' or 'No'):");
 				String yorn = in.next();
 				in.nextLine();
@@ -70,29 +73,35 @@ public class BankAccountMain
 					yorn = in.next();
 					in.nextLine();
 				}
+				
+				//Initial Deposit
 				if(yorn.equals("Yes"))
 				{
 					System.out.println("What would you like to initially deposit: ");
-					//Might need to fix this while loop
-					//This function is messed up
-					while(!isNumeric(in.next()) || in.nextDouble() < 0)
+					String intAmt = in.next();
+					in.nextLine();
+					
+					while(!isNumeric(intAmt))
 					{
 						System.out.println("Please enter a valid answer");
+						intAmt = in.next();
+						in.nextLine();
 					}
-					double intAmt = in.nextDouble();
+
 					
 					if(saveorcheck.equals("Savings"))
 					{
-						accounts.add(new SavingsAccount(name, intAmt, RATE, MIN_BAL, MIN_BAL_FEE));
+						accounts.add(new SavingsAccount(name, Double.parseDouble(intAmt), RATE, MIN_BAL, MIN_BAL_FEE));
 					}
 					
 					else if(saveorcheck.equals("Checking"))
 					{
-						accounts.add(new CheckingAccount(name, intAmt, OVER_DRAFT_FEE, TRANSACTION_FEE, FREE_TRANSACTIONS));
+						accounts.add(new CheckingAccount(name, Double.parseDouble(intAmt), OVER_DRAFT_FEE, TRANSACTION_FEE, FREE_TRANSACTIONS));
 					}
 						
 				}
 				
+				//Without Initial Deposit
 				else if(yorn.equals("No"))
 				{
 					if(saveorcheck.equals("Savings"))
@@ -107,56 +116,88 @@ public class BankAccountMain
 				
 			}
 			
+			//Make a Transaction
 			if(ans.equals("Make"))
 			{
-				System.out.println("What type of transaction would you like to make?(Options: 'Deposit', 'Withdraw', 'Transfer', 'Get Account Number'): ");
+				System.out.println("What type of transaction would you like to make?(Options: 'Deposit', 'Withdraw', 'Transfer', 'Account'): ");
 				String trans = in.next();
 				in.nextLine();
 
-				while((!trans.equals("Deposit")) && (!trans.equals("Withdraw")) && (!trans.equals("Transfer")) && (!trans.equals("Get")))
+				while((!trans.equals("Deposit")) && (!trans.equals("Withdraw")) && (!trans.equals("Transfer")) && (!trans.equals("Account")))
 				{
 					System.out.println("Please enter a valid answer:");
 					trans = in.next();
 					in.nextLine();
 				}
-				switch(trans) 
+				
+				if((trans.equals("Deposit")) && (trans.equals("Withdraw")) && (trans.equals("Transfer")))
 				{
-				case("Deposit"):
-				{
+					//Get Account Number
 					System.out.println("What is your account number?: ");
-					String acc = in.next();
+					String accNum = in.next();
 					in.nextLine();
 					
+					while(!isNumeric(accNum))
+					{
+						System.out.println("Please enter a valid answer: ");
+						accNum = in.next();
+						in.nextLine();
+					}
+				}
+				
+				//Switch case of different transaction types
+				switch(trans) 
+				{
+				
+				//Deposit case
+				case("Deposit"):
+				{
 					System.out.println("How much would you like to depsoit:");
 					String damt = in.next();
 					in.nextLine();
 					
+					while(!isNumeric(damt))
+					{
+						System.out.println("Please enter a valid answer: ");
+						damt = in.next();
+						in.nextLine();
+					}
 					
 					try
 					{
 						//Parsing
-						accounts.get(Integer.parseInt(acc)).deposit(Double.parseDouble(damt));
+						//Need to fix finding account
+						accounts.get(Integer.parseInt(accNum)).deposit(Double.parseDouble(damt));
 					}
 					catch(IllegalArgumentException e)
 					{
 						System.out.println("Transaction not authorized.");
 					}
+					break;
 				}
+				
+				//Withdraw case
 				case("Withdraw"):
 				{
 					
 				}
+				
+				//Transfer case
 				case("Transfer"):
 				{
 					
 				}
-				case("Get"):
+				
+				//Account case
+				case("Account"):
 				{
 					
 				}
 				}
 			}
 			
+			
+			//Terminate the program
 			if(ans.equals("Terminate"))
 			{
 				System.out.println("Thank you for using the Bank Account Program!");
@@ -181,3 +222,5 @@ public class BankAccountMain
 //int.parseInt
 
 //double
+
+//Instance Of
